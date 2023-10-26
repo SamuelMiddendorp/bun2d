4eac4d59487434b6bfd3d320283a627bbbf0f0f4
Samuel Middendorp <samiezaurus@gmail.com>
2023-10-26 21:49:29 +0200

    Some art

--- main2.c
+++ main2.c
@@ -352,7 +352,6 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         clearPixels();
-        //fractal(400,100);
         int angle = sin(glfwGetTime()) * 200;
 
         for(int i = 0; i < 1000; i += 10){
