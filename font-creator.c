3a43af0a382b9c87adce50a790ff7f26c085e985
Samuel Middendorp <samiezaurus@gmail.com>
2023-12-21 20:35:57 +0100

    Feature: very simple font creator

--- font-creator.c
+++ font-creator.c
@@ -3,13 +3,17 @@
 
 int main()
 {
-    bun2dInit(0, 400,400,400,400);
+    bun2dInit(0, 5,5,400,400);
     Pixel color = {255,0,0,255};
     while (bun2dTick())
     {
         if(bun2dKey(KEY_W) == 1 || bun2dKey(KEY_W) == 2){
             Point foo = bun2dGetMouse();
             bun2dPixel(foo.x, foo.y, color);
+        }
+        if(bun2dKey(KEY_D) == 1 || bun2dKey(KEY_D) == 2){
+            Point foo = bun2dGetMouse();
+            bun2dClearPixel(foo.x, foo.y);
         }
         if(bun2dKey(KEY_S) == 1){
             for(int x = 0; x<5 ; x++){
