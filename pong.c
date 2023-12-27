a7b7002b8f301b3f5836b0ed4ee75852db3b78bb
Samuel Middendorp <samiezaurus@gmail.com>
2023-12-27 20:30:41 +0100

    Render paddles

--- pong.c
+++ pong.c
@@ -1,12 +1,27 @@
 #define BUN2D_IMPLEMENTATION 
 #include <bun2d.h>
 
+typedef struct{
+    int posY;
+    int score;
+} Player;
+
 int main()
 {
+    int buffY = 400;
+    int buffX = 400;
+    int paddleHeight = 50;
+    int paddleWidth = 10;
+
     printf("Pong!");
-    bun2dInit(1, 400, 400, 400, 400);
+
+    Player p1 = {100,0};
+    Player p2 = {100,0};
+    bun2dInit(1, buffX, buffY, 400, 400);
     while (bun2dTick())
     {
         bun2dClear();
+        bun2dRect(0,p1.posY, paddleWidth, paddleHeight);
+        bun2dRect(buffY - paddleWidth - 1 , p2.posY, paddleWidth, paddleHeight);
     }
 }
