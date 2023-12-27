e17037f2cf3878a7436774de3773be3d8e13b30a
Samuel Middendorp <samiezaurus@gmail.com>
2023-12-27 20:34:43 +0100

    Get a ball on screen

--- pong.c
+++ pong.c
@@ -6,21 +6,37 @@ typedef struct{
     int score;
 } Player;
 
+typedef struct{
+    int posX;
+    int posY;
+    int velX;
+    int velY;
+} Ball;
+
 int main()
 {
     int buffY = 400;
     int buffX = 400;
+
+    Ball ball = {100,100,2,2};
+
     int paddleHeight = 50;
     int paddleWidth = 10;
 
-    printf("Pong!");
-
     Player p1 = {100,0};
     Player p2 = {100,0};
+
+    printf("Pong!");
     bun2dInit(1, buffX, buffY, 400, 400);
     while (bun2dTick())
     {
         bun2dClear();
+        // Input
+
+
+        // Render ball
+        bun2dCircle(ball.posX, ball.posY, 5);
+        // Render players
         bun2dRect(0,p1.posY, paddleWidth, paddleHeight);
         bun2dRect(buffY - paddleWidth - 1 , p2.posY, paddleWidth, paddleHeight);
     }
