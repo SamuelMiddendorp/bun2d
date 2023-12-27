69dc4442aaed94992c25453be3cbd4f3f58c65a3
Samuel Middendorp <samiezaurus@gmail.com>
2023-12-27 20:42:36 +0100

    Make ball system

--- pong.c
+++ pong.c
@@ -13,12 +13,14 @@ typedef struct{
     int velY;
 } Ball;
 
+void updateBall(Ball* ball, int wallX, int wallY);
+
 int main()
 {
     int buffY = 400;
     int buffX = 400;
 
-    Ball ball = {100,100,2,2};
+    Ball ball = {100,100,4,2};
 
     int paddleHeight = 50;
     int paddleWidth = 10;
@@ -33,11 +35,25 @@ int main()
         bun2dClear();
         // Input
 
-
+        // Logic
+        updateBall(&ball, 400, 400);
         // Render ball
         bun2dCircle(ball.posX, ball.posY, 5);
         // Render players
         bun2dRect(0,p1.posY, paddleWidth, paddleHeight);
         bun2dRect(buffY - paddleWidth - 1 , p2.posY, paddleWidth, paddleHeight);
     }
+}
+
+void updateBall(Ball* ball, int wallX, int wallY){
+    if(ball->posX >= wallX || ball->posX <= 0){
+        ball->velX = -ball->velX;
+    }
+
+    if(ball->posY >= wallY || ball->posY <= 0){
+        ball->velY = -ball->velY;
+    }
+
+    ball->posX += ball->velX;
+    ball->posY += ball->velY;
 }
