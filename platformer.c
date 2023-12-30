2200605172608bab9e23ef212b91ad17cf4953e6
Samuel Middendorp <samiezaurus@gmail.com>
2023-12-30 15:50:13 +0100

    Shooter :)

--- platformer.c
+++ platformer.c
@@ -30,35 +30,46 @@ int bulletTimer = 0;
 
 void makePlatform(int posX, int posY, int width, int height);
 void fireBullet(int x, int y);
+void updateGamestate();
 
 int main()
 {
+    float speed = 0.5;
     bun2dInit(1, 100, 100, 1000, 1000);
 
     Player player = {{5, 0}, {0, 0}, {1, 2}};
-    makePlatform(2, 2, 2, 1);
+    makePlatform(5, 5, 5, 3);
+    makePlatform(5, 10, 5, 3);
+    makePlatform(5, 15, 5, 3);
+    makePlatform(7, 11, 5, 3);
+    makePlatform(12, 12, 5, 3);
 
     while (bun2dTick())
     {
-        if (bun2dKey(KEY_D) == BUN2D_PRESS)
+        bun2dClear();
+        if (bun2dKey(KEY_D) > 0)
         {
-            player.position.x++;
+            player.velocity.x+=speed;
         }
 
-        if (bun2dKey(KEY_A) == BUN2D_PRESS)
+        if (bun2dKey(KEY_A) > 0)
         {
-            player.position.x--;
+            player.velocity.x-=speed;
         }
 
         if (bun2dKey(KEY_W) > 0)
         {
-            if(bulletTimer > 5){
+            if (bulletTimer > 5)
+            {
                 fireBullet(player.position.x, player.position.y);
             }
             bulletTimer = 0;
         }
+        player.position.x += player.velocity.x;
+        player.position.y += player.velocity.y;
 
-        bun2dClear();
+        player.velocity.x = 0;
+        player.velocity.y = 0;
 
         // Render platforms;
         for (int i = 0; i < currentPlatAmount; i++)
@@ -71,12 +82,14 @@ int main()
         {
             bullets[i].position.y++;
             Bullet b = bullets[i];
-            bun2dCircle(b.position.x, b.position.y, 5, RED);
+            bun2dFillRect(b.position.x, b.position.y, 1, 1, RED);
         }
+        updateGamestate();
 
         bun2dFillRect(player.position.x, player.position.y, player.dims.x, player.dims.y, BLUE);
         bulletTimer++;
     }
+    return 0;
 }
 
 void makePlatform(int posX, int posY, int width, int height)
@@ -90,12 +103,32 @@ void fireBullet(int x, int y)
     Bullet b = {{x, y}};
     if (currentBulletAmount == 100)
     {
-        if(currentBulletIndex == 100){
+        if (currentBulletIndex == 100)
+        {
             currentBulletIndex = 0;
         }
         bullets[currentBulletIndex++] = b;
     }
-    else{
+    else
+    {
         bullets[currentBulletAmount++] = b;
+    }
+}
+
+void updateGamestate()
+{
+    for(int i = 0; i < currentPlatAmount; i++){
+        for(int j = 0; j < currentBulletAmount; j++){
+            Platform p = platforms[i];
+            Bullet b = bullets[j];
+            if(b.position.x >= p.position.x
+            && b.position.x <= p.position.x + p.dims.x
+            && b.position.y >= p.position.y
+            && b.position.y <= p.position.y + p.dims.y)
+            {
+                Platform newPlat = {{rand() % 100, rand() % 100 + 5}, {5,3}};
+                platforms[i] = newPlat;
+            }
+        }
     }
 }
