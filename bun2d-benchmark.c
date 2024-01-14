99225f367d7d055497425dd1e910d36ba81901d1
Samuel Middendorp <samiezaurus@gmail.com>
2024-01-14 21:03:43 +0100

    Add gradient rendering to bun2d-benchmark

--- bun2d-benchmark.c
+++ bun2d-benchmark.c
@@ -1,4 +1,4 @@
-#define BUN2D_IMPLEMENTATION 
+#define BUN2D_IMPLEMENTATION
 #include <bun2d.h>
 #define MAX_BUNS 100000
 
@@ -7,12 +7,12 @@ int main()
     int boundX = 1000;
     int boundY = 1000;
     bun2dInit(0, boundX, boundY, 1000, 1000);
-    Model* bun = bun2dLoadModel("bun.csv");
-    int* buns = malloc(sizeof(int) * 2 * MAX_BUNS);
-    for (int i = 0; i < MAX_BUNS * 2; i+=2)
+    Model *bun = bun2dLoadModel("bun.csv");
+    int *buns = malloc(sizeof(int) * 2 * MAX_BUNS);
+    for (int i = 0; i < MAX_BUNS * 2; i += 2)
     {
-        buns[i]= 5 + rand() % boundX - 5;
-        buns[i + 1]= 5 + rand() % boundY - 5;
+        buns[i] = 5 + rand() % boundX - 5;
+        buns[i + 1] = 5 + rand() % boundY - 5;
     }
 
     int frameTimer = 0;
@@ -20,27 +20,34 @@ int main()
     {
         bun2dClear();
         bun2dDrawModelBulk(bun, MAX_BUNS, buns);
-        if(frameTimer > 100){
+        if (frameTimer > 100)
+        {
             printf("frametime: %f ms", bun2dGetFrameTime());
             frameTimer = 0;
             break;
         }
         frameTimer++;
     }
-    Pixel p = {255,255,0,255};
-    while(bun2dTick()){
+    Pixel p = {0, 0, 0, 255};
+    while (bun2dTick())
+    {
         bun2dClear();
-        for(int i = 0; i < boundX; i++){
-
-        for(int j = 0; j < boundY; j++){
-            bun2dPixel(i,j,p);
+        for (int i = 0; i < boundX; i++)
+        {
+            p.r = ((float)255/boundX) * i;
+            for (int j = 0; j < boundY; j++)
+            {
+                p.b = ((float)255/boundX) * j;
+                bun2dPixel(i, j, p);
+            }
         }
-        }
-        if(frameTimer > 100){
+        p.r = 0;
+        p.b = 0;
+        if (frameTimer > 100)
+        {
             printf("frametime: %f ms", bun2dGetFrameTime());
             frameTimer = 0;
         }
         frameTimer++;
     }
-
 }
