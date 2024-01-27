a968f4c6f3c53406d1f74ada52467eaf21a06144
Samuel Middendorp <samiezaurus@gmail.com>
2024-01-27 22:13:59 +0100

    Arbitrairy world

--- examples/raycaster.c
+++ examples/raycaster.c
@@ -3,14 +3,12 @@
 #include <stdbool.h>
 int main()
 {
-    unsigned short resX = 500;
-    unsigned short resY = 500;
-    bool world[20 * 20];
+    bool world[50 * 50];
     Vec2 pos = {5, 5};
     // init world
-    for (int i = 0; i < 20 * 20; i++)
+    for (int i = 0; i < 50 * 50; i++)
     {
-        if (i % 2 == 0)
+        if (rand() % 100 > 98)
         {
             world[i] = true;
         }
@@ -19,43 +17,28 @@ int main()
             world[i] = false;
         }
     }
-    float radians = 0;
-    Pixel color = {255, 100, 100, 255};
+    Pixel color = {255, 50, 50, 255};
 
-    bun2dInit(1, resX, resY, 800, 800);
-    int rays = 50;
-    int length = 50;
+    bun2dInit(1, 50, 50, 800, 800);
     while (bun2dTick())
     {
         bun2dClear();
-        if(bun2dKey(KEY_D) > 1){
-            pos.x+=1;
-        }
-        if(bun2dKey(KEY_A) > 1){
-            pos.x-=1;
-        }
-        for (int i = 0; i < rays; i++)
-        {
-            Vec2 headingVec = {cos(radians), sin(radians)};
-            headingVec.x *= length;
-            headingVec.y *= length;
-            Vec2 target = {headingVec.x + pos.x, headingVec.y + pos.y};
-            bun2dLine(pos.x, pos.y, target.x, target.y, color);
-            radians+=0.05;
-        }
-        radians = 0;
-        // for (unsigned short i = 0; i < resX; i++)
+        // for (int i = 0; i < rays; i++)
         // {
-
-        //     for (unsigned short j = 0; j < resY; j++)
-        //     {
-        //         if(i < 20 && j < 20){
-        //             if(world[20 * i + j]){
-        //                 Pixel p = {255,100,100,255};
-        //                 bun2dPixel(i,j,p);
-        //             }
-        //         }
-        //     }
+        //     Vec2 headingVec = {cos(radians), sin(radians)};
+        //     headingVec.x *= length;
+        //     headingVec.y *= length;
+        //     Vec2 target = {headingVec.x + pos.x, headingVec.y + pos.y};
+        //     bun2dLine(pos.x, pos.y, target.x, target.y, color);
+        //     radians+=0.05;
         // }
+        for(int i = 0; i < 50; i++){
+        for(int j = 0; j < 50; j++){
+            if(world[50 * i + j]){
+                bun2dPixel(i,j,color);
+            }
+        }
+
+        }
     }
 }
