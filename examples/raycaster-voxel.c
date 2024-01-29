2624acc3317814e48e82dfcc973506afbbf26327
Samuel Middendorp <samiezaurus@gmail.com>
2024-01-29 23:22:31 +0100

    Almost there

--- examples/raycaster-voxel.c
+++ examples/raycaster-voxel.c
@@ -1,13 +1,15 @@
 #define BUN2D_IMPLEMENTATION
 #include <bun2d.h>
 
-int toWorldArray(int x, int y, int z);
-float toRad(float degrees);
-
 typedef struct
 {
     float x, y, z
 } Vec3;
+
+int toWorldArray(int x, int y, int z);
+float toRad(float degrees);
+float getMag(Vec3 vec);
+
 
 #define PI 3.14159265358979323846
 #define WORLD_DIM 10
@@ -20,7 +22,7 @@ int main()
     bool world[WORLD_SIZE];
     for (int i = 0; i < WORLD_SIZE; i++)
     {
-        if (rand() % 1000 > 900)
+        if (rand() % 1000 > 950)
         {
             world[i] = true;
         }
@@ -30,20 +32,17 @@ int main()
         }
     }
 
-    Vec3 pos = {0, 0, 5};
+    Vec3 pos = {0, 0, 0};
     bun2dInit(1, screenWidth, screenHeight, 800, 800);
-    int maxRays = 50;
+    int maxRays = 100;
     while (bun2dTick())
     {
-        float fov = toRad(120);
-        float radStep = fov / screenWidth;
-
         bun2dClear();
         if(bun2dKey(KEY_W) > 0){
-            pos.z-=0.1;
+            pos.z-=0.01;
         }
         if(bun2dKey(KEY_S) > 0){
-            pos.z+=0.1;
+            pos.z+=0.01;
         }
         if(bun2dKey(KEY_A) > 0){
             pos.x+=0.01;
@@ -51,22 +50,54 @@ int main()
         if(bun2dKey(KEY_D) > 0){
             pos.x-=0.01;
         }
-        // Cast rays for every pixel on the screen
-        float radY = toRad(120);
+        if(bun2dKey(KEY_Q) > 0){
+            pos.y+=0.01;
+        }
+        if(bun2dKey(KEY_E) > 0){
+            pos.y-=0.01;
+        }
+
         for (int y = 0; y < screenHeight; y++)
         {
-            float radX = toRad(120);
             for (int x = 0; x < screenWidth; x++)
             {
+                Vec2 uv;
+                uv.x = (x * 0.5 - screenWidth) / screenWidth;
+                uv.y = (y * 0.5 - screenHeight) / screenHeight;
+
+
                 // Cast ray
                 // _ and y
-                Vec2 headingY = {cos(radY), sin(radY)};
-                // x and z
-                Vec2 headingX = {cos(radX), sin(radX)};
+                // Vec2 headingY = {cos(radY), sin(radY)};
+                // // x and z
+                // Vec2 headingX = {cos(radX), sin(radX)};
+                Vec3 headingBar;
+                headingBar.x = uv.x;
+                headingBar.y = uv.y;
+                headingBar.z = 1;
+
+                float mag = getMag(headingBar);
+
+                headingBar.x /= mag;
+                headingBar.y /= mag;
+                headingBar.z /= mag;
+                // Calculate heading vector;
+
+                Vec3 newHeading;
+                newHeading.x = headingBar.x - pos.x;
+                newHeading.y = headingBar.y - pos.y;
+                newHeading.z = headingBar.z - pos.z;
+
+                float mag2 = getMag(newHeading);
+
+                newHeading.x /= mag2;
+                newHeading.y /= mag2;
+                newHeading.z /= mag2;
+
 
                 for (int r = 0; r < maxRays; r++)
                 {
-                    Vec3 heading3 = {headingY.x * (r + 1), headingY.y * (r + 1), headingX.y * (r + 1)};
+                    Vec3 heading3 = {newHeading.x * (r + 1), newHeading.y * (r + 1), newHeading.z * (r + 1)};
                     Vec3 newWorldPos = {pos.x + heading3.x, pos.y + heading3.y, pos.z + heading3.z};
                     int worldDim = toWorldArray(newWorldPos.x, newWorldPos.y, newWorldPos.z);
                     if (newWorldPos.x > WORLD_DIM || newWorldPos.y > WORLD_DIM || newWorldPos.z > WORLD_DIM ){
@@ -76,24 +107,29 @@ int main()
                     {
                         Pixel c = RED;
                         c.r = c.r / (r + 1);
-                        bun2dPixel(screenWidth - x, screenHeight - y, c);
+                        bun2dPixel(x, y, c);
                         break;
                     }
+                    // printf("[%f,%f,%f]\n", newWorldPos.x, newWorldPos.y, newWorldPos.z);
+                    // break;
                 }
                 // Debug
                 // bun2dLine(screenWidth / 2, screenHeight / 2, pos.x + headingX.y * 10, pos.y + headingY.x * 10, BLUE);
-                radX += radStep;
             }
-            radY += radStep;
         }
+        // break;
     }
 }
 int toWorldArray(int x, int y, int z)
 {
-    return (z * WORLD_DIM * WORLD_DIM) + (y * WORLD_DIM) + x;
+    return (WORLD_DIM * WORLD_DIM * z) + (WORLD_DIM * y) + x;
 }
 
 float toRad(float degrees)
 {
     return PI * degrees / 180;
+}
+
+float getMag(Vec3 vec){
+    return sqrt(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
 }
